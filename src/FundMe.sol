13c9a9eb3c25456c9c3b05f02cb5267cdab0d199
nik-rawat <nr2192004@gmail.com>
2025-03-03 11:25:38 +0530

    added some new test and mocks

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import { PriceConvertor } from "./PriceConvertor.sol";
 
 error FundMe__NotOwner();
@@ -9,38 +9,37 @@ error FundMe__NotOwner();
 contract FundMe {
     using PriceConvertor for uint256;
 
-    AggregatorV3Interface internal dataFeed;
-
-    uint256 public constant MINIMUM_USD = 5e18;
-
-    address [] public funders;
-    mapping (address funder => uint amountFunded) public addressToAmountFunded;
+    mapping (address funder => uint amountFunded) private s_addressToAmountFunded;
+    address [] private s_funders;
 
     address public immutable i_owner;
+    uint256 public constant MINIMUM_USD = 5e18;
+    AggregatorV3Interface private s_priceFeed;
 
-    constructor () {
-        dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+
+    constructor (address priceFeed) {
+        s_priceFeed = AggregatorV3Interface(priceFeed);
         i_owner = msg.sender;
     }
 
     function getVersion() public view returns (uint256) {
-        return dataFeed.version();
+        return s_priceFeed.version();
     }
 
     function fund() public payable {
         require( msg.value.getConversionRate() >= MINIMUM_USD, "Didn't sent enough ETH");
-        funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] += msg.value.getConversionRate();
+        s_funders.push(msg.sender);
+        s_addressToAmountFunded[msg.sender] += msg.value.getConversionRate();
     }
 
     function withdraw() public onlyOwner {
 
         // require(msg.sender == i_owner, "Must be owner");
-        for(uint funderIndex=0; funderIndex < funders.length; funderIndex++) {
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
+        for(uint funderIndex=0; funderIndex < s_funders.length; funderIndex++) {
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
         }
-        funders = new address[](0);
+        s_funders = new address[](0);
 
         // //transfer
         // payable (msg.sender).transfer( address(this).balance );
@@ -70,5 +69,15 @@ contract FundMe {
         fund();
     }
 
+    /*
+    View / Pure Functions (Getters)
+     */
 
+    function getAddressToAmountFunded(address fundingAddress) external view returns (uint256) {
+        return s_addressToAmountFunded[fundingAddress];
+    }
+
+    function getFunder(uint256 index) external view returns (address) {
+        return s_funders[index];
+    }
 }
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -3,12 +3,21 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
+    address USER = makeAddr("user");
+
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STATING_BALANCE = 10 ether;
+
     function setUp() external {
-        fundMe = new FundMe();
+        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+        vm.deal(USER, STATING_BALANCE); 
     }
 
     function testMinDollorIsFive() public view {
@@ -16,11 +25,23 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsMsgSender() public view {
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
     }
 
     function testPriceFeedVersionIsAccurate() public view {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughEth() public {
+        vm.expectRevert();
+        fundMe.fund();
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
