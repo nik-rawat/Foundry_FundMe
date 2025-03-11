813cba09451b0ddef29a1c7204b79883c48524e8
nik-rawat <nr2192004@gmail.com>
2025-03-11 16:06:06 +0530

    completed project

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,18 +1,18 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
 import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 
-import {FundMe} from "../src/FundMe.sol";
-
 contract FundFundMe is Script {
-    uint256 SEND_VALUE = 0.01 ether;
+    uint256 SEND_VALUE = 0.1 ether;
 
-    function fundFundMe(address mostRecentlyDeployed) external{
+    function fundFundMe(address mostRecentlyDeployed) public {
         vm.startBroadcast();
-        FundMe(payable (mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
         vm.stopBroadcast();
+        console.log("Funded FundMe with %s", SEND_VALUE);
     }
 
     function run() external {
@@ -22,16 +22,15 @@ contract FundFundMe is Script {
 }
 
 contract WithdrawFundMe is Script {
-
-    function withdrawFundMe(address mostRecentlyDeployed) external{
+    function withdrawFundMe(address mostRecentlyDeployed) public {
         vm.startBroadcast();
         FundMe(payable(mostRecentlyDeployed)).withdraw();
         vm.stopBroadcast();
+        console.log("Withdraw FundMe balance!");
     }
 
     function run() external {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
-        WithdrawFundMe(mostRecentlyDeployed);
+        withdrawFundMe(mostRecentlyDeployed);
     }
-    
 }
